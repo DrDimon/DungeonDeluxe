46bcc514b6b93351e6bc0061fea031a1e9de499b
Stunax <cmollgaard@gmail.com>
2016-02-06 15:57:52 +0100

    Basic unit scripts

---
+++ Assets/Scripts/Interaction/Attack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack {
+    private int dmg;
+
+    void init(int dmg)
+    {
+        this.dmg = dmg;
+    }
+
+    void attackMod(Being target)
+    {
+
+    }
+
+    void deffMod(Being attacker)
+    {
+
+    }
+}
---
+++ Assets/Scripts/Monsters/Monster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Monster : Being{
+    public GameObject player;
+    public int health;
+
+    //returns amount of dmg done.
+    int attack(GameObject target)
+    {
+        return 0;
+    }
+
+    void recieveDmg(int dmg)
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
